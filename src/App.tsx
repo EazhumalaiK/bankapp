230a9b58fa2c8273e2eef0639271a1f4169f3aa0
eazhumalaikaliyaperumal <eazhumalai.kaliyaperumal@ariyalsoft.in>
2025-01-25 08:44:37 +0000

    # initial app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline p-4 m-30 text-orange-800">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
